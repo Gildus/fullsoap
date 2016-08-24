2e57b4c35b827de29a8e25c3eb282a39fa594865
Gildus <gildus@gmail.com>
2016-08-23 23:04:54 -0300

    Initial files

---
+++ app/Http/Controllers/RestfullController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class RestfullController extends Controller
+{
+    public function getSuma()
+	{
+		die('jijijiji');
+	}
+	
+	public function postSuma()
+	{
+	
+	}
+	
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Service RestFull and Soap with Laravel
+
